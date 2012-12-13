e5a8903a384a7e8119138840947e0fab253e7c01
IIUJ-BartoszMigacz <bartosz.migacz@uj.edu.pl>
2012-12-13 14:39:26 +0100

    Dodano algorytm obliczania predkosci kol. Dodano klienta TCP/IP do komunikacji z serwerem na robocie.

---
+++ objectTracking-wheelControl/WheelController.cpp
@@ -0,0 +1,52 @@
+/*
+ * WheelController.cpp
+ *
+ *  Created on: 11-12-2012
+ *      Author: arch
+ */
+
+#include "WheelController.h"
+#include <iostream>
+#include <cmath>
+
+#define _USE_MATH_DEFINES
+
+
+using namespace std;
+
+WheelController::WheelController() {
+	// TODO Auto-generated constructor stub
+
+}
+
+WheelController::~WheelController() {
+	// TODO Auto-generated destructor stub
+}
+
+
+std::pair<int, int> WheelController::getSpeeds(float angle) const {
+	const float d = 30;
+	const int speed = 10000;
+	const float w = 5; // distance between a wheel and the center of the robot
+
+	bool to_right = angle > 0;
+
+	angle = abs(angle);
+	float alpha = (M_PI/2) - angle;
+	float r = 0.5 * d / cos(alpha);
+	float l_to_r = (r+w)/(r-w);
+	//cout << "r=" << r << "; " << "ltor=" << l_to_r << endl;
+	if(!to_right)
+		l_to_r = 1./l_to_r;
+	pair<int, int> speeds = make_pair(l_to_r * speed, (1./l_to_r) * speed);
+	return speeds;
+}
+
+int main() {
+	WheelController wc;
+	pair<int,int> sr, ml;
+	sr = wc.getSpeeds(0);
+	ml = wc.getSpeeds(-1);
+	cout << sr.first << ", " << sr.second << endl;
+	cout << ml.first << ", " << ml.second;
+}
