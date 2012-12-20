004855f12a1d062fec993d1a6e510e9c4ed79c37
Bartosz Migacz <bartosz.migacz@uj.edu.pl>
2012-12-20 15:48:45 +0100

    dodano brakujace pliki dot. sterowania kolami

--- objectTracking/main.cpp
+++ objectTracking/main.cpp
@@ -5,32 +5,32 @@
 using namespace cv;
 using namespace std;
 
-int main(int argc, char** argv) {
-	bool finish = false;
-
-	/* Kolo Naukowe Robotyki's Camera */
-	//string rtspAddress = "rtsp://192.168.0.106:554/live.sdp";
-	/* Testing Camera */
-	string rtspAddress = "rtsp://212.77.7.133:80/h264lan.sdp";
-
-	KheperaView* kheperaView = new KheperaView(rtspAddress);
-	if (kheperaView->start() < 0) {
-		printf("Camera is null\n");
-		finish = true;
-	}
-
-	while (!finish) {
-		kheperaView->capture();
-
-
-		//kheperaView->show_gray_view();
-		kheperaView->show_main_panel();
-
-
-		finish = kheperaView->is_stoped();
-	}
-
-	waitKey(0);
-	cvDestroyAllWindows();
-	return 0;
-}
+//int main(int argc, char** argv) {
+//	bool finish = false;
+//
+//	/* Kolo Naukowe Robotyki's Camera */
+//	//string rtspAddress = "rtsp://192.168.0.106:554/live.sdp";
+//	/* Testing Camera */
+//	string rtspAddress = "rtsp://212.77.7.133:80/h264lan.sdp";
+//
+//	KheperaView* kheperaView = new KheperaView(rtspAddress);
+//	if (kheperaView->start() < 0) {
+//		printf("Camera is null\n");
+//		finish = true;
+//	}
+//
+//	while (!finish) {
+//		kheperaView->capture();
+//
+//
+//		//kheperaView->show_gray_view();
+//		kheperaView->show_main_panel();
+//
+//
+//		finish = kheperaView->is_stoped();
+//	}
+//
+//	waitKey(0);
+//	cvDestroyAllWindows();
+//	return 0;
+//}
